437f3e34ac1b450eb308cfdbe7dbb13d65a4d91d
apporoad <apporoadina@163.com>
2020-07-29 00:18:29 +0800

    some

--- test.js
+++ test.js
@@ -17,3 +17,46 @@ var hello = async ()=>{
 }
 
 hello()
+
+
+var splitCsvRow = function(rowString){
+  var cols = []
+  var tempString = rowString
+  while(tempString.length>0){
+      value = ""
+      if(tempString[0] == "\""){
+          var index = tempString.indexOf("\",")
+          if(index>-1){
+              value = tempString.substring(1,index)
+              tempString = tempString.substring(index +2)
+          }else{
+              if(tempString[tempString.length-1] == '"'){
+                  value = tempString.substring(1,tempString.length-1)
+                  tempString =""
+              }
+              else{
+                  throw Error('error csv row :' + tempString)
+              }
+          }
+      }else{
+          var index = tempString.indexOf(",")
+          if(index>-1){
+              value = tempString.substring(0,index)
+              tempString = tempString.substring(index +1)
+          }else{
+              value = tempString
+              tempString = ""
+          }
+      }
+      cols.push(value.replace(/""/g,"\""))
+  }
+  return cols
+}
+
+
+//"sdf	bsf"	sdfsdf,sfsdf	"sfsdf,sdfsd	sdfd,sdf"
+//csv :  """sdf",bsf","sdfsdf,sfsdf","""sfsdf,sdfsd","sdfd,sdf"""
+
+console.log(splitCsvRow('"sdf	bsf"	sdfsdf,sfsdf	"sfsdf,sdfsd	sdfd,sdf"'))
+
+console.log(splitCsvRow('"""sdf",bsf","sdfsdf,sfsdf","""sfsdf,sdfsd","sdfd,sdf"""'))
